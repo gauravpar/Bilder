a07784c24add0de688587e24728c26f46080fcf2
Vagelis <vanguelisk@gmail.com>
2014-04-06 19:07:57 +0300

    An Early version of the xml backup glyphbook

--- WortVerbinden/src/xmltest.py
+++ WortVerbinden/src/xmltest.py
@@ -4,30 +4,88 @@ Created on Apr 4, 2014
 This is just a test module to store and load xml
 In the xml you will store the char  the path to its glyph ,if it is a special char its dimensions the top , low line and baseline
 The xml works as a backup in case everything is lost
+The xml contains all the chars contains the the listwidget
 @author: phoenix
 '''
- 
-chars=['a','b']
+from xml.etree.ElementTree import ElementTree 
+from xml.etree.ElementTree import Element
+import xml.etree.ElementTree as etree
+
+chars=['α','ρ']
 Lang=["GR","GR"]
-path=["/tmp/a.png","/tmp/r.png"]
-spec=["No","Yes"]
+Path=["/tmp/a.png","/tmp/r.png"]
+Spec=["No","Yes"]
 Top=["8","12"]
 Low=["28","32"]
 Base=["28","24"]
 X=["35","40"]
 Y=["45","49"]
 
+root=Element("GlyphBook")
+tree=ElementTree(root)
+i=0
+for ch in chars:
+    glyph=Element("Glyph")
+    root.append(glyph)
+    c=Element("Char")
 
-root=et.Element("root")
-
-for c in chars:
-    glyph=et.SubElement(root,"Glyph")
-    glyph.set("name", "α")
-    glyph.text = "some value1"
+    glyph.append(c)
+    c.text= ch
     
-tree = et.ElementTree(root)
+    
+    spec=Element("Special")
+    spec.text=Spec[i]
+    glyph.append(spec)
+    
+    
+    p=Element("Path")
+    p.text=Path[i]
+    glyph.append(p)
+
+    l=Element("Lang")
+    l.text=Lang[0]
+    glyph.append(l)
+    
+    top=Element("Top")
+
    
-tree.write("filename.xml", xml_declaration=True, encoding='utf-8')    
-tree.write("/tmp/filename.xml")
+    top.text=Top[i]
+    glyph.append(top)
+    
+    low=Element("Low")
+
+   
+    low.text=Low[i]
+    glyph.append(low)
+    
+    b=Element("Base")
+    b.text=Base[i]
+    glyph.append(b)
+    
+   
+    x=Element("X")
+    x.text=X[i]
+    glyph.append(x)
+    
+   
+   
+   
+   
+    y=Element("Y")
+    y.text=Y[i]
+    glyph.append(y)
+    
+    
+    
+    
+   
+   
+    
+    i+=1
+
+
+print etree.tostring(root, encoding='utf-8')
+
+tree.write("/tmp/filename.xml", xml_declaration=True,encoding='utf-8' )    
 print 'ok'
 
