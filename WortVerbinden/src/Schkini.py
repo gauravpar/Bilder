9ce2f0273accd86762230b6e106619df859fabb2
Vagelis <vanguelisk@gmail.com>
2014-04-06 21:44:47 +0300

    A custom QGraphicsScene that will allow the user to cut glyphs and store them to his disk

---
+++ WortVerbinden/src/Schkini.py
@@ -0,0 +1,16 @@
+'''
+Created on Apr 6, 2014
+
+@author: phoenix
+'''
+from PyQt4.QtGui import QGraphicsScene
+#If the user is to click and select glyphs then an extended qgraphics scene class is needed
+#if you can't do it it is already coded in C++
+
+class Schkini(QGraphicsScene):
+
+
+
+    def __init__(self, params):
+        pass
+        
