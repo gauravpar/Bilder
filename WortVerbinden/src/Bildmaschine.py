4810a3934a572fb28a7c6b57e29dfc795f859893
Vagelis <vanguelisk@gmail.com>
2014-04-14 13:57:22 +0300

    Das Herz von Buchstabebildersystem

---
+++ WortVerbinden/src/Bildmaschine.py
@@ -0,0 +1,39 @@
+'''
+Created on Apr 14, 2014
+
+@author: phoenix
+'''
+
+class MyClass():
+    #This is the main buchstabebilder engine
+   
+   
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+     
+     
+    def Verarbeit(self):
+        #Pre process char image
+        
+        
+        
+    def Otzu(self):
+        #Otzu Binarization
+        
+    def FindBaseLine(self):
+        #Finds the baseline
+        #Take special care for some chars
+        
+    
+    def Concat(self):
+        #concatenate chars
+        
+    def EstimateLetterSpace(self):
+        #Estimates in a very naïve way the space between chars
+        
+    def EstimateWordSpace(self):
+        #Estimates in a very naïve way the space between words
+   
