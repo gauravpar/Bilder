b43f5b0460c896c6f598357035096ec70fa69f2e
Vagelis <vanguelisk@gmail.com>
2014-04-21 14:24:19 +0300

    2014 fixes

--- WortVerbinden/src/skewtest.py
+++ WortVerbinden/src/skewtest.py
@@ -6,8 +6,12 @@ Created on Apr 15, 2014
 import cv2,math
 import numpy as np
 from numpy import array # we ll use to calc linear regression equation
-vorher=cv2.imread('/home/phoenix/Verbinden/testhist.png',cv2.CV_LOAD_IMAGE_GRAYSCALE)
+vorher=cv2.imread('/home/phoenix/Verbinden/testskew1.png',cv2.CV_LOAD_IMAGE_GRAYSCALE)
+color=cv2.imread('/home/phoenix/Verbinden/testskew1.png',cv2.CV_LOAD_IMAGE_COLOR)
+
+
 #Elina 's way
+#the skew seems correct but the line detection does not
 
 
 
@@ -21,16 +25,24 @@ Height, Width=vorher.shape
 x=[]
 y=[]
 
+
 for i in reversed(range(0,Height)):
     for j in range(0,Width):
         #get black pixels
         if otzu[i][j]==0:
             y.append(i)
             x.append(j)
+            #color these pixels BLUE
+            color[x][y][0]=255
+            color[x][y][1]=0
+            color[x][y][2]=0
+            
             break
     else:
         continue
 
+
+cv2.imshow('Colored dots',color)
 print ('Black pixels are:')
 for i in range(0,len(x)):
     print x[i],y[i]
@@ -38,7 +50,8 @@ for i in range(0,len(x)):
      
 a,b=np.polyfit(x, y, 1)
 
-#print a
+print 'a',a,'b',int(b) #b seems to be the upper main body line
+
 rads=math.atan(a)
 degs=(rads*180)/math.pi #it seems to WORK
 print "angle in degrees",degs
@@ -58,7 +71,7 @@ rot=cv2.warpAffine(otzu,M,(Width,Height))
 
 
 cv2.imshow("Rotated",rot)
-
+cv2.imwrite("/tmp/rotated.png",rot)
 
 cv2.waitKey()
 cv2.destroyAllWindows()
