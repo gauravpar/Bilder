d3b680cade96ddb1d10edd85103ea8e2c73c5102
Vagelis <vanguelisk@gmail.com>
2014-04-21 20:36:14 +0300

    working merged with Bildmaschine

--- WortVerbinden/src/Bildmaschine.py
+++ WortVerbinden/src/Bildmaschine.py
@@ -12,45 +12,91 @@ class BildMaschine():
     def __init__(self, q,bildbook):
         self.Query=q
         self.BilderBuch=bildbook
-
+        self.LetterSpace=21
 
         
     def Anfangen(self):
         
         
         print 'Synthesizing begins...'
-        print 'String is',self.Query
+        print 'String Query is',self.Query.replace('\n','')
         print 'Breaking query into chars:'
-        print ('Lines are')
         #Image Query Dimensions
-        QueryX=0
-        QueryY=0
+        QueryW=0
+        QueryH=0
         #find the maximum X
-        print 'Image Query Height',QueryX
-        for c in self.Query:
-          
-         
-            for gl in self.BilderBuch:
-                if c is gl.Char:
-                    print gl.Char
-                    print gl.Top
-                    print gl.Low
-                    print gl.BaseLine
-                    QueryY+=gl.width
-                    if gl.Height>QueryX:
-                        QueryX=gl.Height
-                        
-        
-        QueryX*=2
+        print 'Image Query Height',QueryH
+        print 'Printing Glyphbook'
+        for gl in self.BilderBuch:
+            print '----------------------------'
+            print gl.Char
+            print 'Width',gl.Width
+            print 'Height',gl.Height
+            print 'Left Line',gl.Left
+            print 'Right Line',gl.Right
+            print 'Top Line',gl.Top
+            print 'Low Line',gl.Low
+            print 'Base line',gl.BaseLine
+            QueryW+=gl.Width+self.LetterSpace #!!!!!
+            if gl.Height>QueryH:
+                QueryH=gl.Height
+                
+    
+        QueryH*=2
                 
         print 'Let \'s go'
-        print 'Image Query Height',QueryX
-        print 'Image Query Width',QueryY
+        print 'Image Query Height',QueryH
+        print 'Image Query Width',QueryW
+        
+      
+        ImgQuery=np.ones((QueryH,QueryW,1), np.uint8)
+        ImgQuery.fill(255)   #A white image
+     
+
+
+        Shift=2
+        
+        s_row=0+Shift
+        s_col=0
+        
+        BaseLine=0 #the baseline is the line of the first char
+        CharCount=0
         
         for c in self.Query:
+            print '------------------------------'
             print 'Next char to be app',c
+            #find it in the glyphbook
+            for gl in self.BilderBuch:
+                if str(c) in gl.Char: #find the char in the glyphbook
+                    print 'It will be loaded from',gl.GraphemeImg
+                    #all in one line...
+                    s_row=abs(gl.BaseLine-BaseLine)+Shift
+                   
+                    
+                    CharCount+=1
+                    
+                    if CharCount==1:
+                        BaseLine=gl.BaseLine
+                        s_row=Shift
+                    
+                    self.Concat(s_col, s_row, ImgQuery, gl.Naher,gl.Left,gl.Right,gl.Height)
+                    
+                    s_col+=+self.LetterSpace+gl.Right
+                    
             
+        print 'Finished'
+        cv2.imwrite('/tmp/query.png',ImgQuery)
+    
+    
+    def Concat(self,StartCol,StartRow,qpic_arr,char_arr,links,rechts,ypsos):
+        #concatenate chars
+        print 'Appending a glyph w to r',StartRow,'c',StartCol
+        for i in range(0, ypsos):
+            for j in range(links, rechts):
+                qpic_arr[i+StartRow][j+StartCol]=char_arr[i][j]
  
+            
+   
 
     def Erode(self,img,kernel):
         return cv2.erode(img,kernel,iterations=1)
@@ -71,11 +117,7 @@ class BildMaschine():
         return cv2.warpAffine(pic_arr,M,(w,h))   
         
     
-    
-        
-    #def Concat(self):
-    #    #concatenate chars
-        
+  
     #def EstimateLetterSpace(self):
     #    #Estimates in a very naïve way the space between chars
         
